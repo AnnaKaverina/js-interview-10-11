f7d65d21a7a14abd395e796b2da260df3e0729e0
Anna <aik303030@gmail.com>
2022-11-10 16:16:36 +0300

    feat: adding fetchGracePeriodLogicFromMapping logic

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const _ = require('lodash')
 
+// variant without using lodash
+
 function fetchGracePeriodLogicFromMapping (cardId, mapping = '') {
-    return "base"
+    const regExpId = new RegExp('\\D' + cardId + '\\D', 'g');
+    const matchIds = mapping.match(regExpId);
+    if (matchIds === null) return null;
+    const indexTo = mapping.indexOf(matchIds[matchIds.length - 1]) + 1;
+    const substring = mapping.slice(0, indexTo);
+    const regExpName = new RegExp(/[A-Za-z]+/g);
+    const matchNames = substring.match(regExpName);
+    return matchNames[matchNames.length - 1];
+
 }
 
 module.exports = fetchGracePeriodLogicFromMapping
